9578b1553afcf6b5f734a3230beab35ef8f84636
sraaths <sraaths@uchicago.edu>
2024-10-23 20:18:15 +0000

    c1

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/")do
+"welcome"
 end
+
+get("/square/new")do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  erb(:square_results)
+  @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num ** 2 
+end
+
+get("square_root/new")do
+  erb(:square_root_new)
+end
+
+get("square_root/results")do 
+  @root = params.fetch("root_number").to_f
+  @root_result = @root **2
+end 
