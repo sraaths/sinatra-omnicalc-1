6772db92cc3d15eea25bc85c97d286c6412dd635
sraaths <sraaths@uchicago.edu>
2024-10-23 20:41:12 +0000

    c2

--- app.rb
+++ app.rb
@@ -15,11 +15,25 @@ get("/square/results") do
   @the_result = @the_num ** 2 
 end
 
-get("square_root/new")do
+get("/square_root/new")do
   erb(:square_root_new)
 end
 
-get("square_root/results")do 
+get("/square_root/results")do 
   @root = params.fetch("root_number").to_f
   @root_result = @root **2
 end 
+
+get("/payment/new")do
+  erb(:payment_new)
+end
+
+get("/payment/results")do
+  erb(:payment_results)
+@apr = params.fetch("apr").to_f / 100
+@years = params.fetch("years").to_f
+@principal = params.fetch("principal").to_f
+@monthly_rate = @apr / 12
+@number_months = @years * 12
+@payment_result = @principal * (@monthly_rate * (1 + @monthly_rate)** @number_months) / ((1 + @monthly_rate)** @number_months - 1)
+end
